20ec7e32115a537415fd4bd691e4f09ec40a78cc
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2022-01-13 10:49:58 +0700

    edit

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,70 +1,47 @@
 class AppsController < ApplicationController
-  before_action :set_app, only: %i[ show edit update destroy ]
-
-  # GET /apps or /apps.json
+  before_action :set_user, only: [ :show, :edit, :destroy, :update]
   def index
     @app = App.all
   end
 
-  # GET /apps/1 or /apps/1.json
-  def show
-  end
-
-  # GET /apps/new
   def new
     @app = App.new
   end
 
-  # GET /apps/1/edit
-  def edit
+  def show
   end
 
-  # POST /apps or /apps.json
   def create
-    @app = App.new(app_params)
-
-    respond_to do |format|
-      if @app.save
-        format.html { redirect_to app_url(@app), notice: "App was successfully created." }
-        format.json { render :show, status: :created, location: @app }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @app.errors, status: :unprocessable_entity }
-      end
-    end
+    @app = App.new(post_params)
+    if @app.save
+      # redirect_to notice:"succsessful"
+    else
+      render :new
+    end 
   end
 
-  # PATCH/PUT /apps/1 or /apps/1.json
+  def edit
+
+  end
+
   def update
-    respond_to do |format|
-      if @app.update(app_params)
-        format.html { redirect_to app_url(@app), notice: "App was successfully updated." }
-        format.json { render :show, status: :ok, location: @app }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @app.errors, status: :unprocessable_entity }
-      end
+    if @app.update(post_params)
+      # redirect_to  notice:"succsessful"
+    
+    else
+      render :edit
     end
   end
 
-  # DELETE /apps/1 or /apps/1.json
   def destroy
     @app.destroy
-
-    respond_to do |format|
-      format.html { redirect_to apps_url, notice: "App was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    #  redirect_to  notice:"destroy ----"
   end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_app
-      @app = App.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def app_params
-      params.require(:app).permit(:name, :date)
-    end
+  def set_user
+    @app = App.find(params[:id])
+  end
+  def post_params
+    params.require(:app).permit(:name, :date)
+  end
 end
--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,3 @@
 class App < ApplicationRecord
-  
+    validates :name, length: { maximum: 14 }
 end
